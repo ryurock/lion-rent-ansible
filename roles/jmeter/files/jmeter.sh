cf0a42dc117e4b4d7289c5dec4fe14461ea61eaa
ryurock <ryusukekimura3@gmail.com>
2014-05-13 01:53:46 +0900

    first commit

---
+++ roles/jmeter/files/jmeter.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+if [ -d /usr/local/jmeter/bin ]; then
+    export PATH="/usr/local/jmeter/bin:$PATH"
+fi
---
+++ roles/php/php-build/install-php/templates/phpenv.sh
@@ -0,0 +1,6 @@
+export RBENV_ROOT=/usr/local/rbenv
+export PATH="$RBENV_ROOT/bin:$PATH"
+eval "$(rbenv init -)"
+
+export PATH="/usr/local/phpenv/bin:$PATH"
+eval "$(phpenv init -)"
